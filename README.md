6396cbb8fa475291248e4e540002755cd193f9d6
Guest User <Guest@epicodus-118.local>
2018-03-30 16:43:40 -0700

    add README.md

---
+++ README.md
@@ -0,0 +1,34 @@
+# WEEK THREE CODE REVIEW
+
+#### _Beep Boop website, 03/23/2018_
+
+## Description
+This program replaces certain numbers given by the user with the phrases "Beep!" "Boop!" and "I'm sorry Dave, I'm afraid I can't do that."
+
+## Specs
+* _Numbers that contain a 0 are replaced with "beep!"_
+  * _Ex. input: 20; Ex. Output: "beep!"_
+* _Numbers that contain a 1 are replaced with "boop!"_
+  * _Ex. input: 15; Ex. output: "boop!"_
+* _Numbers that are divisible by 3 are replaced with "I'm sorry Dave, I'm afraid I can't do that."_
+  * _Ex. input: 15; Ex. output: "I'm sorry Dave, I'm afraid I can't do that."_
+* _These rules should be followed top to bottom, so that user input=30 would give the third result._
+  * _Ex. input: 10; Ex. output: "boop!"_
+
+## Setup/Installation
+* _Clone the repository from Github._
+* _Open the index.html file in your browser._
+
+## Known Bugs
+* _None, currently._
+
+## Technology Used
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _JavaScript_
+* _jQuery_
+
+### License
+_Copyright (c) 2018 Andrew Lupton_
+_This software is licensed under the GPL license._
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 //define variables
 var userOutput;
 var userInput;
+var userArray =[];
 
 //transform function
-function(userInput) {
-  if (number%3 !== 0 || number === 0) {
-    return "Beep!"
-  } else if (number.includes (1) && number%3 !==0 || number == 1) {
-    return "Boop!"
-  } else if (number%3 ===0) {
-    return "I'm sorry Dave, I'm afraid I can't do that."
+function output(userInput) {
+  if (userInput%3 !== 0 || userInput === 0) {
+    return "Beep";
+  } else if (number.includes (1) && userInput%3 !==0 || userInput == 1) {
+    return "Boop";
+  } else if (userInput%3 ===0) {
+  return "I'm sorry Dave, I'm afraid I can't do that.!";
+
   }
 };
 
@@ -20,16 +22,18 @@ $(document).ready(function() {
 
     //take input from user
     var userInput = parseInt($("input#number").val());
-    var userOutput = output;
+    var userOutput = output(userInput);
     //bounce non-numbers
-    if(!userInput) {
-      alert ("That's not a valid number");
-    }
 
 
     //show result
     $("#output").text(userOutput);
     $("#result").show();
+    if(!userInput) {
+      alert ("That's not a valid number");
+      return "";
+    }
+
     //hide result
     $("#hideResult").click(function(){
           $("#result").hide();
