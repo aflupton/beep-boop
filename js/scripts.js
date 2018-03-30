ebc6a3e3c7650554338a92895110f24a750e163b
Guest User <Guest@epicodus-118.local>
2018-03-30 16:32:05 -0700

    add fix to NaN alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,39 @@
+//define variables
 var userOutput;
 var userInput;
-var outputArray = [];
 
+//transform function
+function(userInput) {
+  if (number%3 !== 0 || number === 0) {
+    return "Beep!"
+  } else if (number.includes (1) && number%3 !==0 || number == 1) {
+    return "Boop!"
+  } else if (number%3 ===0) {
+    return "I'm sorry Dave, I'm afraid I can't do that."
+  }
+};
 
 
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
 
-  function output(userInput) {
-    for (var i=0; i<userInput; i++) {
-        if (userInput%3 === 0) {
-        while ((number.includes(1)) || (number%3 ==0)) {
-          userInput = "Beep!"}
-      } else if (number.includes(1)){
-          while (userInput%3 ==0){
-          userOutput = "Boop!"}
-      } else if (userInput%3 == 0){
-          userOutput = "I'm sorry Dave, I'm afraid I can't do that.";
-      }
+    //take input from user
+    var userInput = parseInt($("input#number").val());
+    var userOutput = output;
+    //bounce non-numbers
+    if(!userInput) {
+      alert ("That's not a valid number");
     }
-  }
-    var userInput = parseInt($("form#formInput").val());
-    var userOutput = output(userInput);
 
 
-
-    $("#output").text(userInput);
+    //show result
+    $("#output").text(userOutput);
     $("#result").show();
+    //hide result
     $("#hideResult").click(function(){
-          $("#result").fadeOut("slow");
-          });
+          $("#result").hide();
+    });
+    //end
   });
 });
