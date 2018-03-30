37d1b4f5ed24577feb53630e437fcc892fe00928
Guest User <Guest@epicodus-118.local>
2018-03-30 15:49:21 -0700

    add jquery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
+var userOutput;
 var userInput;
+var outputArray = [];
 
 
 
@@ -6,8 +8,28 @@ $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("form#formInput").val();
+  function output(userInput) {
+    for (var i=0; i<userInput; i++) {
+        if (userInput%3 === 0) {
+        while ((number.includes(1)) || (number%3 ==0)) {
+          userInput = "Beep!"}
+      } else if (number.includes(1)){
+          while (userInput%3 ==0){
+          userOutput = "Boop!"}
+      } else if (userInput%3 == 0){
+          userOutput = "I'm sorry Dave, I'm afraid I can't do that.";
+      }
+    }
+  }
+    var userInput = parseInt($("form#formInput").val());
+    var userOutput = output(userInput);
+
 
 
     $("#output").text(userInput);
     $("#result").show();
+    $("#hideResult").click(function(){
+          $("#result").fadeOut("slow");
+          });
+  });
+});
